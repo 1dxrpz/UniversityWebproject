d552fbdd91d11f018a4d817245e09dc73857d581
Kenneth Gibson <scaphoidederps@gmail.com>
2021-12-07 16:29:04 +0300

    feat(ORM): created orm

---
+++ AnimeWebproject/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Models;
+using DxORM;
+using DxORM.Services;
+
+namespace AnimeWebproject
+{
+	public class ApplicationContext : DbContext
+	{
+		public DbSet<User> Users;
+		public ApplicationContext() : base()
+		{
+			Users = new DbSet<User>("Users", this);
+		}
+		public override void OnCreate()
+		{
+			//AddTable<User>(Users);
+		}
+	}
+}
---
+++ AnimeWebproject/Pages/Home/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using AnimeWebproject.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimeWebproject.Pages
+{
+	public class IndexModel : PageModel
+	{
+		public string test = "Hello world";
+
+		HomeController controller;
+		public IndexModel(ApplicationContext context)
+		{
+			controller = new HomeController(context);
+		}
+		public PageResult Get()
+		{
+			return Page();
+		}
+	}
+}
