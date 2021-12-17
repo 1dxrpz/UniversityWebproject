5ad134969d19d1ea002d25799ca855fd6584629d
Kenneth Gibson <scaphoidederps@gmail.com>
2021-12-18 02:35:16 +0300

    fix(orm)

---
+++ AnimeWebproject/Models/Video.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AnimeWebproject.Models
+{
+	public class Video
+	{
+		public Guid Id { get; set; }
+		public String Link { get; set; }
+		public String Name { get; set; }
+		public String Description { get; set; }
+	}
+}
